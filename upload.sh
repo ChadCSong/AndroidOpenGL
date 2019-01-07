21360dc7d87e26549b3a810ae6c5d315db657b16
liyachao <liyachao.knight@bytedance.com>
2019-01-07 19:42:49 +0800

    feauture:update the gradle and sh file

--- upload.sh
+++ upload.sh
@@ -7,23 +7,23 @@ then
     echo "please input \"./upload.sh player \" or \"./upload.sh glview \" "
     exit 1
 fi
- 
+
 verion="PLAYER_TOOL_VERSION"
-moduleName="alpha-mp4-player"
+moduleName=alpha-mp4-player
 if [[ "$module" == "glview" ]]
 then
     echo "success"
     version="GLVIEW_TOOL_VERSION"
-    moduleName="multiple-stream-glview"
+    moduleName=multiple-stream-glview
 fi
 echo "version: $verion"
 echo "moduleName: $moduleName"
 
 # 查找TOOL_VERSION字符
-origin=`grep ${version} gradle.properties | awk '{print $1}'`
-notNeedChange=`grep ${version} gradle.properties | cut -d '.' -f 1`
-secondVersion=`grep ${version} gradle.properties | cut -d '.' -f 2`
-lastVersion=`grep ${version} gradle.properties | cut -d '.' -f 3 `
+origin=`grep $verion gradle.properties | awk '{print $1}'`
+notNeedChange=`grep $verion gradle.properties | cut -d '.' -f 1`
+secondVersion=`grep $verion gradle.properties | cut -d '.' -f 2`
+lastVersion=`grep $verion gradle.properties | cut -d '.' -f 3 `
 
 str=${notNeedChange}"."${secondVersion}"."$[lastVersion+1]
 echo ${str}
@@ -33,7 +33,7 @@ echo "begin upload please wait!!!~~~"
 
 bintaryUser=`grep "PbintrayUser" local.properties | cut -d '=' -f 2`
 bintaryKey=`grep "PbintrayKey" local.properties | cut -d '=' -f 2`
-./gradlew clean :${moduleName}:build :${moduleName}:bintrayUpload -PbintrayUser=${bintaryUser} -PbintrayKey=${bintaryKey} -PdryRun=false 2>&1 | tee log.txt
+./gradlew clean :$moduleName:build :$moduleName:bintrayUpload -PbintrayUser=${bintaryUser} -PbintrayKey=${bintaryKey} -PdryRun=false 2>&1 | tee log.txt
 
 uploadStr=`grep "BUILD SUCCESSFUL" log.txt`
 
